51af392f4ab8043e0199cd816e9d6f5b5cac260f
dejvi <dejviepadejvi@gmail.com>
2023-02-07 16:06:40 +0100

    shtuam projektin

---
+++ src/components/data.js
@@ -0,0 +1,205 @@
+const products = [
+    {
+      id: 1,
+      name: 'Iphone 14 Pro Max',
+      rating: 4.9,
+      description: 'Latest Iphone Out Now',
+      price: 1899,
+      image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+    },
+    {
+        id: 2,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 3,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 4,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 5,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 6,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 7,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 8,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 9,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 10,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 11,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 12,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 13,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 14,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 15,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 16,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 17,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 18,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 19,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 20,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 21,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 22,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 23,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 24,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+      {
+        id: 25,
+        name: 'Iphone 14 Pro Max',
+        rating: 4.9,
+        description: 'Latest Iphone Out Now',
+        price: 1899,
+        image: 'https://assets.shpresa.al/shop/2022/09/daac540d-cel1351-p.jpg'
+      },
+    
+]
+
+export default products;
